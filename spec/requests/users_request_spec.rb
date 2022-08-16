941da17ac25499ad5d72aa2cb606e695a3e1d752
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-08-16 22:35:19 +0200

    Add tests for users controller

---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'users', type: :request do
+  context 'testing users#index' do
+    it 'returns http success' do
+      get '/users'
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the correct view' do
+      get '/users'
+      expect(response).to render_template :index
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/users'
+      expect(response.body).to include 'List of all users'
+    end
+  end
+
+  context 'testing users#show' do
+    it 'returns http success' do
+      get '/users/:id'
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the correct view' do
+      get '/users/:id'
+      expect(response).to render_template :show
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/users/:id'
+      expect(response.body).to include 'Profile page for a given user'
+    end
+  end
+  
+end
