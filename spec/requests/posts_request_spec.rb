0c8bf2bf7c5a0b1d7ea5c7c764b21f8196d15a76
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-08-16 22:36:12 +0200

    Add tests for posts controller

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'posts', type: :request do
+  context 'testing posts#index' do
+    it 'returns http success' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the correct view' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template :index
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/users/:user_id/posts'
+      expect(response.body).to include 'list of posts for a given user'
+    end
+  end
+
+  context 'testing posts#show' do
+    it 'returns http success' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the correct view' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template :show
+    end
+
+    it 'response body includes correct placeholder text' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include 'specific post for a given user'
+    end
+  end
+  
+end
