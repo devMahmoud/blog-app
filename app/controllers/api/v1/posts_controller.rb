716c8aaaa3f12cf2bea558af93ee911ecd595ca7
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-08-27 01:24:10 +0200

    Add authentication to API endpoints.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,4 @@
-class  Api::V1::PostsController < ApplicationController
+class Api::V1::PostsController < ApplicationController
   def index
     @user_posts = Post.where(author_id: params[:user_id]).order(created_at: :desc)
     render json: @user_posts
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_with_token
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -6,5 +7,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+
+  def authenticate_with_token
+    if params[:api_token]
+      user = User.find_by_api_token(params[:api_token])
+      sign_in(user)
+    end
   end
 end
