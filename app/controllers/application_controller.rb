c5dbf253692f8933664d56387f9690f4d830ce49
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-08-27 01:49:00 +0200

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,8 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_with_token
-    if params[:api_token]
-      user = User.find_by_api_token(params[:api_token])
-      sign_in(user)
-    end
+    return nil unless params[:api_token]
+    user = User.find_by_api_token(params[:api_token])
+    sign_in(user)
   end
 end
